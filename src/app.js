bd080c844333a2fa5101d6ec14fe1e543bff2c3c
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-04-27 15:06:15 -0300

    criacao do banco de dados

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from "express";
+import {openDb} from "./config/ConfigDb.js"
+
+openDb();
 
 const app = express();
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send('Projeto M4');
   }) 
-
+app.post('/pessoa', (req, res) => {
+  console.log("req.body");
+  res.json({
+    "StatusCode": 200
+  })
+})
 
 
 
