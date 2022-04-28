b00c61600e1a3972ee064b6a883e93cf2848744d
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-04-28 17:57:17 -0300

    Criacao de servico https e SSL

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import app from './src/app.js'
+import fs from "fs";
+import https from "https";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`servidor escutando em http://localhost:${port}`)
 })
+
+https.createServer({
+    cert: fs.readFileSync('./src/SSL/code.crt'),
+    key: fs.readFileSync('./src/SSL/code.key')
+}, app).listen(3001,()=> console.log('rodando em https'));
--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { openDb } from "./config/ConfigDb.js";
 import { createTableFuncionario } from "./controllers/Funcionario.js";
+import cors from "cors";
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 import router from "./routers/routes.js"
 app.use(router);
+
 
 export default app;
