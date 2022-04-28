66a94d16f438fb38c39e359a2ca3b8e12c14f410
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-04-27 21:13:57 -0300

    Criacao rota put e delete

--- src/app.js
+++ src/app.js
@@ -1,23 +1,59 @@
 import express from "express";
-import {openDb} from "./config/ConfigDb.js"
-import {createTable, insertCliente, updateCliente} from "./controllers/Cliente.js"
+import { openDb } from "./config/ConfigDb.js";
+import {
+  createTable,
+  insertCliente,
+  updateCliente,
+  selectClientes,
+  selectCliente,
+  deleteCliente
+} from "./controllers/Cliente.js";
 createTable();
 
-const app = express(); 
+const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send('Projeto M4');
-  }) 
-app.post('/cliente', (req, res) => {
+app.get("/", (req, res) => {
+  res.status(200).send("Projeto M4");
+});
+
+app.get("/clientes",async function (req, res) {
+  let pessoas = await selectClientes();
+  res.json(pessoas);
+});
+
+app.get("/cliente",async function (req, res) {
+  let pessoa = await selectCliente(req.body.id);
+  res.json(pessoa);
+});
+
+
+
+app.post("/cliente", (req, res) => {
   insertCliente(req.body);
   res.json({
-    "StatusCode": 200
-  })
-})
+    StatusCode: 200,
+  });
+});
 
+app.put("/cliente", (req, res) => {
+  if (req.body && !req.body.id) {
+    res.json({
+      statusCode: "400",
+      msg: "Voce precisa informar um id",
+    });
+  } else {
+    updateCliente(req.body);
+    
+    res.json({
+      StatusCode: 200,
+    });
+  }
+});
 
+app.delete("/cliente",async function (req, res) {
+  let pessoa = await deleteCliente(req.body.id);
+  res.json(pessoa);
+});
 
-
-
-  export default app;
+export default app;
--- src/controllers/Cliente.js
+++ src/controllers/Cliente.js
@@ -3,20 +3,21 @@ import{openDb} from "../config/ConfigDb.js";
 export async function createTable(){
     openDb()
         .then(db => {
-        db.exec ('CREATE TABLE IF NOT EXISTS Cliente (id INTEGER PRIMARY KEY, nome TEXT,cpf TEXT,idade INTEGER, email TEXT,endereço TEXT, genero TEXT, telefone INTEGER)')
+        db.exec ('CREATE TABLE IF NOT EXISTS Cliente (id INTEGER PRIMARY KEY, nome TEXT,cpf TEXT,idade INTEGER, email TEXT,endereco TEXT, genero TEXT, telefone INTEGER)')
     })
 }
 
 export async function insertCliente(cliente){
+    console.log(cliente)
     openDb()
         .then(db => {
-        db.run ('INSERT INTO Cliente (nome,cpf,idade,email,endereço,genero,telefone) VALUES (?,?,?,?,?,?,?)',
+        db.run ('INSERT INTO Cliente (nome,cpf,idade,email,endereco,genero,telefone) VALUES (?,?,?,?,?,?,?)',
         [
         cliente.nome,
         cliente.cpf,
         cliente.idade,
         cliente.email,
-        cliente.endereço,
+        cliente.endereco,
         cliente.genero,
         cliente.telefone
         ]);
@@ -24,17 +25,45 @@ export async function insertCliente(cliente){
 }
 
 export async function updateCliente(cliente){
+    
     openDb()
         .then(db => {
-        db.run ('UPDATE Cliente SET nome=?,cpf=?,idade=?,email=?,endereço=?,genero=?,telefone=? WHERE id=?',
+        db.run ('UPDATE Cliente SET nome=?,cpf=?,idade=?,email=?,endereco=?,genero=?,telefone=? WHERE id=?',
         [
         cliente.nome,
+        cliente.cpf,
         cliente.idade,
         cliente.email,
-        cliente.endereço,
+        cliente.endereco,
         cliente.genero,
         cliente.telefone,
         cliente.id
         ]);
+    })
+}
+
+export async function selectClientes(cliente){
+    
+    return openDb().then(db => {
+        return db.all ('SELECT * FROM Cliente')
+        .then(res=>res)
+    })
+}
+
+export async function selectCliente(id){
+    
+    return openDb().then(db => {
+        return db.get ('SELECT * FROM Cliente WHERE id=?',
+        [id])
+        .then(res=>res)
+    })
+}
+
+export async function deleteCliente(id){
+    
+    return openDb().then(db => {
+        return db.get ('DELETE FROM Cliente WHERE id=?',
+        [id])
+        .then(res=>res)
     })
 }
