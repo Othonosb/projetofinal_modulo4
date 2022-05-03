06cdd2d04a960cf2aee40c64f516ed75a1d987aa
othonosb <othonosb@hotmail.com>
2022-05-03 10:10:13 -0300

    atualização de rotas

--- src/controllers/hardwareController.js
+++ src/controllers/hardwareController.js
@@ -1,46 +1,20 @@
 import db from '../data/ConfigDB.js';
-import Hardware from '../models/Hardware';
-import HardwareDAO from '../DAO/hardwareDAO';
+import Hardware from '../models/Hardware.js';
+import HardwareDAO from '../DAO/hardwareDAO.js';
 import res from 'express/lib/response.js';
 
 const hardwareDAO = new HardwareDAO(db);
 
 export default class HardwareController {
 
-    //Criando a tabela dos Hardware
-    // static async createTableHardware() {
-        // openDb()
-            // .then(db => {
-                // db.exec('CREATE TABLE IF NOT EXISTS Hardware (id INTEGER PRIMARY KEY, nome TEXT, marca TEXT, preço INTEGER, tipo TEXT)')
-            // })
-    // }
-    // //Selecionar todos cadastros
-
-    // static async  selectHardwares(req, res) {
-    // openDb().then(db => {
-        // db.all('SELECT * FROM Hardware')
-            // .then(hardwares => res.json(hardwares))
-    // })
-    // }
-
-    static async selectClientes(req, res) {
+    static async selectHardwares(req, res) {
         try {
-            let hardwares = await hardwareDAO.selectHardware();
+            let hardwares = await hardwareDAO.selectHardwares();
             res.json(hardwares);
         } catch (err) {
             res.status(400).json(err);
         }
     }
-
-    //Seleção por ID do Produtos-Hardwares
-
-    // static async selectHardware(req, res) {
-    // let id = req.params.id;
-
-    // openDb()
-        // .then(db => db.get(`SELECT * FROM Hardware WHERE ID = ?`, [id]))
-        // .then(hardwares => res.json(hardwares))
-    // }
 
     static async selectHardware(req, res) {
         const id = req.params.id;
@@ -54,23 +28,6 @@ export default class HardwareController {
     }
 
 
-    //Inserir novos Hardwares
-
-    // static async  insertHardware(req, res) {
-    // let Hardware = req.body
-    // openDb()
-        // .then(db => {
-            // db.run('INSERT INTO Hardware (nome,marca,preço,tipo) VALUES (?,?,?,?)',
-                // [
-                    // Hardware.nome,
-                    // Hardware.marca,
-                    // Hardware.preço,
-                    // Hardware.tipo,
-                // ]);
-        // })
-    // res.status(200).json("Produto cadastro com sucesso!")
-    // }
-
     static async insertHardware(req, res) {
         const body = req.body;
         const hardware = new Hardware(body.nome, body.marca, body.preço, body.tipo);
@@ -83,25 +40,6 @@ export default class HardwareController {
         }
     }
 
-
-//Implementação do UPDATE dos Produtos-Hardwares
-
-    // static  async  updateHardware(req, res) {
-    // let Hardware = req.body
-    // openDb()
-        // .then(db => {
-            // db.run('UPDATE Hardware SET nome=?,marca=?,preço=?,tipo=? WHERE id=?',
-                // [
-                    // Hardware.nome,
-                    // Hardware.marca,
-                    // Hardware.preço,
-                    // Hardware.tipo,
-                    // Hardware.id
-                // ]);
-        // })
-    // res.status(200).json("Produto atualizado com sucesso!")
-    
-    // }
     static async updateHardware(req, res) {
         const hardware = req.body;
 
@@ -114,22 +52,7 @@ export default class HardwareController {
     }
 
 
-
-
-//Implementação do DELETE dos Produtos-Hardwares
-
-    // static async deleteHardware(req, res) {
-    // let id = req.params.id;
-
-    // openDb()
-        // .then(db => db.get(`DELETE FROM Hardware WHERE ID = ?`, [id]))
-
-    // res.status(200).json("Produto deletado com sucesso!")
-    
-// }
-// }
-
-static async deleteHardware(req, res) {
+    static async deleteHardware(req, res) {
     const id = req.params.id;
 
     try {
@@ -138,5 +61,5 @@ static async deleteHardware(req, res) {
     } catch (error) {
         res.status(400).send(error.message);
     }
-}
-}
+    }
+    }
